bee01367f80efb8ddb1a9b19d7ee7a90cc4380b9
Alexander Bleissem <alexander@bleissem.com>
2022-12-13 21:43:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from skbuild import setup
 
 setup(
-    name='ws2223-group7-hanabi-learning-environment',
-    version='0.0.4',    
-    description='ws2223-group7 Learning environment for the game of hanabi.',
-    long_description='ws2223-group7 Learning environment for the game of hanabi.',
+    name='ws2223-group7-hanabi-learning-environment_hgts',
+    version='0.0.1',    
+    description='ws2223-group7 Learning environment for the game of hanabi hgts agent.',
+    long_description='ws2223-group7 Learning environment for the game of hanabi hgts agent.',
     long_description_content_type="text/markdown",
     author='ws2223-group7/hanabi-learning-environment',
     packages=['hanabi_learning_environment', 'hanabi_learning_environment.agents'],
