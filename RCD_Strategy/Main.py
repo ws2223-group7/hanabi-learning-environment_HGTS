9be5c8455413b779fd4ab07f36e26447bcf8c7c3
Alexander Bleissem <alexander@bleissem.com>
2022-12-19 21:29:50 +0100

    fix module import error

--- RCD_Strategy/Main.py
+++ RCD_Strategy/Main.py
@@ -18,6 +18,12 @@ from __future__ import print_function
 import sys
 import getopt
 
+import os
+
+currentPath = os.path.dirname(os.path.realpath(__file__))
+parentPath = os.path.dirname(currentPath)
+sys.path.append(parentPath)
+
 from hanabi_learning_environment import rl_env
 from hanabi_learning_environment.agents.random_agent import RandomAgent
 from hanabi_learning_environment.agents.simple_agent import SimpleAgent
