1045ee775963a01b3477ab02c4686d0c8c889129
Sascha Steger <s-steger@live.de>
2022-12-19 14:49:37 +0100

    Programmed Method - encode_hint
    New Method
    - hint_hat_sum_smaller_4
    - hint_hat_sum_bigger_3
    
    New Instanzvariable
    - oberservation

--- RCD_Strategy/htgs_agent.py
+++ RCD_Strategy/htgs_agent.py
@@ -12,6 +12,8 @@ class HTGSAgent(Agent):
     self.since_hint_plyd_card = False
     self.rcd_card_plyd = False
 
+    self.observation = None
+
 
     # Get Hat to recomm 
     self.encodeActToHat = {0: {'action_type': 'PLAY', 'card_index': 0},
@@ -25,6 +27,8 @@ class HTGSAgent(Agent):
 
   def act(self, observation):
     """Act based on an observation."""
+    self.observation = observation
+
     rcd_act = self.decode_hint()
 
     if (rcd_act['action_type'] == 'PLAY' and self.rcd_card_not_plyd):
@@ -42,7 +46,7 @@ class HTGSAgent(Agent):
 
     # PlayRule 3
     elif (observation['information_tokens'] != 0):
-      return self.give_Hint()
+      return self.give_Hint(observation)
 
     # PlayRule 4
     elif (rcd_act['action_type'] == 'DISCARD'):
@@ -55,10 +59,50 @@ class HTGSAgent(Agent):
 
 
   def give_hint(self):
-    pass 
+    act_hint = self.encode_hint()
+    return act_hint
             
   def encode_hint(self):
-    pass
+      hatSumMod8 = self.calHatSumMod8()
+      if (hatSumMod8 < 4):
+        hint = self.get_hint_hat_sum_smaller_4(hatSumMod8)
+        return hint 
+
+      else:
+        hint = self.get_hint_hat_sum_bigger_3(hatSumMod8)
+        return hint
+  
+  def hint_hat_sum_smaller_4(self, hatSumMod8):
+    # See Paper Cox for calculation 
+    idxPly = hatSumMod8 + 1
+
+    # Get a random rank to hint from player (idxPly)
+    # which get the hint 
+    hand_player = self.observation['observed_hands'][idxPly]
+    first_hand_card = hand_player[0]
+    rank = first_hand_card['rank']
+
+    hint =  {'action_type': 'REVEAL_Rank',
+            'rank': rank,
+            'target_offset': idxPly }
+
+    return hint
+  
+  def get_hint_hat_bigger_3(self, hatSumMod8):
+    idxPly = hatSumMod8 - 3
+
+    # Get a random color to hint from player (idxPly)
+    # which get the hint 
+    hand_player = self.observation['observed_hands'][idxPly]
+    first_hand_card = hand_player[0]
+    color = first_hand_card['color']
+
+  
+    hint =  {'action_type': 'REVEAL_COLOR',
+            'Color': color,
+            'target_offset': idxPly }
+
+    return hint
 
   def cal_hat_sum_mod8(self):
     pass
