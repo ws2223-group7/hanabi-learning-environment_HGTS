8393ad2289c788b1ab4ea0e5add13f07b2dae4c9
Sascha Steger <s-steger@live.de>
2022-12-19 20:40:29 +0100

    Programmed all Method just not cal_own_hat

--- RCD_Strategy/htgs_agent.py
+++ RCD_Strategy/htgs_agent.py
@@ -16,14 +16,14 @@ class HTGSAgent(Agent):
 
 
     # Get Hat to recomm 
-    self.encodeActToHat = {0: {'action_type': 'PLAY', 'card_index': 0},
-                           1: {'action_type': 'PLAY', 'card_index': 1},
-                           2: {'action_type': 'PLAY', 'card_index': 2},
-                           3: {'action_type': 'PLAY', 'card_index': 3},
-                           4: {'action_type': 'DISCARD', 'card_index': 0},
-                           5: {'action_type': 'DISCARD', 'card_index': 1},
-                           6: {'action_type': 'DISCARD', 'card_index': 2},
-                           7: {'action_type': 'DISCARD', 'card_index': 3}}
+    self.encode_act_to_hat = {0: {'action_type': 'PLAY', 'card_index': 0},
+                              1: {'action_type': 'PLAY', 'card_index': 1},
+                              2: {'action_type': 'PLAY', 'card_index': 2},
+                              3: {'action_type': 'PLAY', 'card_index': 3},
+                              4: {'action_type': 'DISCARD', 'card_index': 0},
+                              5: {'action_type': 'DISCARD', 'card_index': 1},
+                              6: {'action_type': 'DISCARD', 'card_index': 2},
+                              7: {'action_type': 'DISCARD', 'card_index': 3}}
 
   def act(self, observation):
     """Act based on an observation."""
@@ -49,8 +49,10 @@ class HTGSAgent(Agent):
       return self.give_Hint(observation)
 
     # PlayRule 4
-    elif (rcd_act['action_type'] == 'DISCARD'):
+    elif (rcd_act['action_type'] == 'DISCARD' and self.rcd_card_not_plyd):
+      self.rcd_card_plyd = True
       return rcd_act
+      
 
     # PlayRule 5
     else:
@@ -64,17 +66,32 @@ class HTGSAgent(Agent):
             
   def encode_hint(self):
       hatSumMod8 = self.calHatSumMod8()
+
+      # if hatSumMod8 < 4 give hint rank (See Paper Cox)
       if (hatSumMod8 < 4):
         hint = self.get_hint_hat_sum_smaller_4(hatSumMod8)
         return hint 
 
+      # if hatSumMod8 > 3 give hint color (See Paper Cox) 
       else:
         hint = self.get_hint_hat_sum_bigger_3(hatSumMod8)
         return hint
   
-  def hint_hat_sum_smaller_4(self, hatSumMod8):
+  def cal_hat_sum_mod8(self):
+      hat_sum_player = 0
+      
+      for idx_ply in range(1, self.observation['num_players']):
+          hand_player = self.observation['observed_hands'][idx_ply]
+          hat_player = self.cal_hat_player(hand_player)
+          hat_sum_player += hat_player
+
+      hat_sum_mod8 = hat_sum_player % 8
+
+      return hat_sum_mod8
+
+  def hint_hat_sum_smaller_4(self, hat_sum_mod8):
     # See Paper Cox for calculation 
-    idxPly = hatSumMod8 + 1
+    idxPly = hat_sum_mod8 + 1
 
     # Get a random rank to hint from player (idxPly)
     # which get the hint 
@@ -84,61 +101,158 @@ class HTGSAgent(Agent):
 
     hint =  {'action_type': 'REVEAL_Rank',
             'rank': rank,
-            'target_offset': idxPly }
+            'target_offset': idxPly } 
 
     return hint
   
   def get_hint_hat_bigger_3(self, hatSumMod8):
-    idxPly = hatSumMod8 - 3
+    # See Paper Cox for calculation 
+    idx_ply = hatSumMod8 - 3
 
     # Get a random color to hint from player (idxPly)
     # which get the hint 
-    hand_player = self.observation['observed_hands'][idxPly]
-    first_hand_card = hand_player[0]
+    hand_ply = self.observation['observed_hands'][idx_ply]
+    first_hand_card = hand_ply[0]
     color = first_hand_card['color']
 
   
     hint =  {'action_type': 'REVEAL_COLOR',
             'Color': color,
-            'target_offset': idxPly }
+            'target_offset': idx_ply }
 
     return hint
 
-  def cal_hat_sum_mod8(self):
-    pass
 
   def cal_hat_player(self, player_hand):
-    pass
+    rules = [self.rule1_hat_player_value,
+             self.rule2_hat_player_value,
+             self.rule3_hat_player_value,
+             self.rule4_hat_player_value,
+             self.rule5_hat_player_value,]
+
+    for rule in rules:
+        hat = rule(player_hand)
+        if (hat is not None):
+          return hat
 
   def rule1_hat_player_value(self, player_hand):
-    pass
+    # Recommend playable Card Rank 5 with lowest Index
+    for idx_card, card in enumerate(player_hand):
+        if card['Rank'] == 4:
+            if self.card_is_playable(card):
+                # Der Hat ist gleich der KartenIndex (Siehe Paper)
+                hat = idx_card
+                return hat
+    return None
 
   def rule2_hat_player_value(self, player_hand):
-    pass
+    min_rank = 999
+    idx_ply_card = 5
+    for idx_card, card in enumerate(player_hand):
+      if (self.card_is_playable(card) and card['rank'] < min_rank):
+        min_rank = card['rank']
+        idx_ply_card = idx_card
+
+    if (min_rank == 999):
+      return None
+
+    hat = idx_ply_card
+    return hat
 
   def rule3_hat_player_value(self, player_hand):
-    pass
+    for idx_card,card in enumerate(player_hand):
+      if (self.cardIsDead(card)):
+          hat = idx_card
+          return hat
+
+      return None
 
   def rule4_hat_player_value(self, player_hand):
-    pass
+    max_rank = -1
+    idx_ply_card = 5
+    for idx_card, card in enumerate(player_hand):
+        if (self.indispensable_card(card) == False and card['rank'] > max_rank):
+            max_rank = card['rank']
+            idx_ply_card = idx_card
+     
+    if (max_rank == -1):
+        return None
+    
+    hat = idx_ply_card
+    return hat
 
   def rule5_hat_player_value(self, player_hand):
-    pass    
+    return 0    
     
   def decode_hint(self):
-    pass
+    # Spielanfang kein encodedHint vorhanden
+      # => Gebe Hint
+      if (self.encode_hint == None):
+          if (self.observation['information_tokens'] >= 1):
+              rcd_act = self.give_hint()
+              
+          else:
+              rcd_act = {'action_type': 'DISCARD', 'card_index': 0}
+              
+
+      else:
+        hat = self.cal_own_hat()
+        rcd_act = self.encode_act_to_hat[hat]
+
+      return rcd_act
 
   def cal_own_hat(self):
     pass
 
-  def check_card_available(self):
-    pass 
+
+
 
   def playable_card(self, card, fireworks):
     """A card is playable if it can be placed on the fireworks pile."""
     return card['rank'] == fireworks[card['color']]
-            
+
+  def dead_card(self, card, firework):
+    if (card['rank'] < firework[card['color']]):
+        return True
+    
+    # Max Karten pro Rank die abgeworfen sein dürfen 
+    max_card_per_rank = {3,2,2,2}
+
+    cards_in_dsc_pile = {0,0,0,0}
+    
+    for card_dsc_pile in self.observation['discard_pile']:
+      # Prüfe alle Karten im dsc_pile mit der selben Farbe und
+      # einem geringen Rank 
+      if (card_dsc_pile['color'] == card['color'] 
+          and card_dsc_pile['rank'] < card['rank']):
+        cards_in_dsc_pile[card_dsc_pile['rank']] += 1    
+
+    # Wenn alle Karten eines rankes einer Farbe abgewurfen worden
+    # dann return True (Karte ist ToT)
+    for idx,elem in enumerate(cards_in_dsc_pile):
+      if elem >= max_card_per_rank[idx]:
+        return True
+
+    return False
+
+  def indispensable_card(self,card):
+
+    # Anzahl der verbleiben Karten
+    # in Deck, Firework und Händen   
+    nr_rem_card_in_deck = {3,2,2,2,1}
+    
+    for card_dsc_pile in self.observation['discard_pile']:
+      # Prüfe alle Karten im dsc_pile mit der selben Farbe und
+      # einem geringen Rank 
+      if card_dsc_pile['color'] == card['color']:
+        nr_rem_card_in_deck[card_dsc_pile['rank']] -= 1    
+
+    # Wenn die Karten nur noch einmal da ist
+    # dann return True (Karte ist ToT)
+    if nr_rem_card_in_deck[card['rank']] == 1:
+      return True
+
+    return False
 
 
-
-        
+    
