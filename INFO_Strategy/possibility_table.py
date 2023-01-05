06ba3025147a96310b48754bb72349bac12ee933
Sascha Steger <s-steger@live.de>
2023-01-05 18:12:26 +0100

    Add Method get_ti() and get_card_table()

--- INFO_Strategy/possibility_table.py
+++ INFO_Strategy/possibility_table.py
@@ -1,8 +1,11 @@
-
+from hanabi_learning_environment import rl_env
+from hanabi_learning_environment.agents.random_agent import RandomAgent
 
 class Table(list):
     def __init__(self, observation):
         super().__init__(self.init_table(observation))
+
+        self.colors = ['B', 'G', 'R', 'W', 'Y']
     
     def init_table(self, observation):
 
@@ -13,17 +16,44 @@ class Table(list):
 
         rep_color = [1,1,1,1,1]
 
-        rep_hand =  {'BLUE': rep_color.copy(),
-                     'GREEN': rep_color.copy(),
-                     'RED': rep_color.copy(),
-                     'WHITE': rep_color.copy(),
-                     'YELLOW': rep_color.copy(),}
+        rep_hand =  {'B': rep_color.copy(),
+                     'G': rep_color.copy(),
+                     'R': rep_color.copy(),
+                     'W': rep_color.copy(),
+                     'Y': rep_color.copy(),}
 
         table = [[rep_hand]*num_cards_per_hand for i in range (num_players)]
 
         return table
+    
+    def get_card(self, player_idx, card_idx):
+        """ Wenn die Karte (player_idx, card_idx)
+        bekannt ist return Card sonst None"""
 
+        # Prüfe ob Karte bekannt ist 
+        if(self.table.get_ti(0, card_idx) == 1):
+            # Ermittel Karte von Table 
+            pass 
 
+    def get_ti(self, player_idx:int, card_idx:int)-> int:
+        """ Bestimme ti (Anzahl von offene Möglichkeiten) 
+        für eine bestimme Karte (player_idx, card_idx) """
+        ti = 0
+        card_table = self.get_card_table(player_idx, card_idx)
+        
+        # Iteriere über alle Farben im Table und summiere liste
+        # P=1 und N=0 ti ist die Anzahl der P's 
+        for color in self.colors: 
+            ti += sum(card_table[color])
+        
+        return ti
+
+    def get_card_table(self, player_idx, card_idx)-> list:
+        """ Return card_table"""
+        card_table = self[player_idx][card_idx] 
+        return card_table
+
+    
 
 
     
@@ -32,9 +62,9 @@ class Table(list):
 
 def get_observation():
 
-    AGENT_CLASSES = {'HTGSAgent' : HTGSAgent}
+    AGENT_CLASSES = {'HTGSAgent' : RandomAgent}
 
-    flags = {'players': 5, 'num_episodes': 100, 'agent_class': 'HTGSAgent'}
+    flags = {'players': 5, 'num_episodes': 100, 'agent_class': 'RandomAgent'}
 
     options, arguments = getopt.getopt(sys.argv[1:], '',
                                     ['players=',
@@ -61,8 +91,7 @@ def get_observation():
 
 
 if __name__ == "__main__":
-    from hanabi_learning_environment import rl_env
-    from htgs_info_agent import HTGSAgent
+
 
     import sys
     import getopt
