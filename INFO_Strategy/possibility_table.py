f716069b00796adc1e316b7fd2a17a8562650741
Sascha Steger <s-steger@live.de>
2023-01-06 15:52:10 +0100

    Add get_card,  get_size_hint_sets, dead_card, get_deads_card_dict

--- INFO_Strategy/possibility_table.py
+++ INFO_Strategy/possibility_table.py
@@ -31,9 +31,15 @@ class Table(list):
         (ti=1) return Card sonst None"""
 
         # Prüfe ob Karte bekannt ist 
-        if(self.table.get_ti(card_table) == 1):
+        if(self.get_ti(card_table) == 1):
             # Ermittel Karte von Table 
-            pass 
+            for color in self.colors:
+                for rank, value in enumerate(card_table[color]):
+                    if value == 1:
+                        card = {'color': color, 'rank': rank}
+                        return card
+
+        return None
 
     def get_ti(self, card_table)-> int:
         """ Bestimme ti (Anzahl von offene Möglichkeiten) 
@@ -56,6 +62,7 @@ class Table(list):
         """ Return hand_table"""
         hand_table = self[player_idx]
         return hand_table
+    
     def get_part_table(self, observation, card_table):
         """Return den partition table zu einem hand_table"""
         
@@ -76,6 +83,94 @@ class Table(list):
 
         # Setze zusätzliche (sieberer) hint sets
         part_table = self.set_seven_hint_sets(part_table, seven_hint_sets) 
+
+
+    def get_deads_card_dict(self, observation)->dict:
+        """Return dict zu toten Karten im Spiel
+        Es wird der max. Rank der toten Karten übergeben"""
+
+        # Erzeuge Liste mit allen Karten 
+        max_rank = 4
+        all_cards = [{'color': color, 'rank':rank} for color in self.colors 
+                                                   for rank in range(max_rank+1)]
+
+        # Prüfe jede Karte ob Sie tot ist 
+        dead_cards = []
+        for card in all_cards:
+            if (self.dead_card(card, observation)):
+                dead_cards.append(card)
+        
+        return dead_cards
+                
+
+    def dead_card(self, card, observation):
+        """Return True wenn Karte tot sonst False"""
+
+        firework = observation['fireworks']
+        if (card['rank'] < firework[card['color']]):
+            return True
+        
+        # Max Karten pro Rank die abgeworfen sein dürfen 
+        max_card_per_rank = [3,2,2,2]
+
+        cards_in_dsc_pile = [0,0,0,0]
+        
+        for card_dsc_pile in observation['discard_pile']:
+        
+            # Prüfe alle Karten im dsc_pile mit der selben Farbe und
+            # einem geringen Rank 
+            if (card_dsc_pile['color'] == card['color'] 
+                and card_dsc_pile['rank'] < card['rank']):
+                cards_in_dsc_pile[card_dsc_pile['rank']] += 1    
+
+            # Wenn alle Karten eines rankes einer Farbe abgewurfen worden
+            # dann return True (Karte ist ToT)
+            for idx,elem in enumerate(cards_in_dsc_pile):
+                if elem >= max_card_per_rank[idx]:
+                    return True
+
+        return False
+    
+    def get_size_hint_sets(self, card_table, dead_cards_dict):
+        """Return die Anzahl an single hint sets  
+        und die Anzahl der hint sets mit größe sieben"""
+        
+        num_dead_card = len(dead_cards_dict)
+
+        # Die max singleton_size hängt davon ob
+        # ob eine Partition von Dead Cards belegt wird 
+        max_single_hint_set = 7 if num_dead_card < 0 else 8
+
+        # Es können alle verbleiben Karten (ti - num_dead_card)
+        # als singleton realisiert werden wenn es nicht zu viele sind
+        single_hint_set = self.get_ti(card_table) - num_dead_card 
+
+        # Anzahl der zusätzlichen 7 hint sets 
+        seven_hint_sets = 0  
+        
+        # Solange es zu viele singleton gibt ziehe ein block der größe 7 ab 
+        while (single_hint_set > max_single_hint_set):
+            single_hint_set -= 7
+            seven_hint_sets += 1
+
+            # Jedes zusätzliche Set reduziert
+            max_single_hint_set -= 1
+
+        return single_hint_set, seven_hint_sets
+
+
+    def set_dead_hint_set(self, part_table, dead_cards_dict):
+        """Return part_table mit gesetzer Partition für alle 
+        dead cards"""
+        pass        
+
+    def set_singleton_hint_sets(part_table, single_hint_sets):
+        """Return part_table mit gesetzen Partitionen 
+        für single hint set"""
+        pass    
+
+        
+
 
 
 
