f51e432a8382bc767c4759302cd6df8867c18007
Sascha Steger <s-steger@live.de>
2023-01-12 15:26:10 +0100

    rl.env.make() & def __init() muss entfernt werden. Sonst wirft python3 -m unittest ein Fehler

--- tests/test_htgs_info_agent.py
+++ tests/test_htgs_info_agent.py
@@ -1,69 +1,108 @@
-from hanabi_learning_environment import rl_env
-from hanabi_learning_environment.agents.random_agent import RandomAgent
-
 import unittest
-import sys
 import os
-import getopt
+import sys
 
 currentPath = os.path.dirname(os.path.realpath(__file__))
 parentPath = os.path.dirname(currentPath)
 sys.path.append(parentPath)
 
+
 from INFO_Strategy.htgs_info_agent import HTGSAgent
-from INFO_Strategy.possibility_table import Table
 
-class TestHTGSAgent(unittest.TestCase):
-    def __init__(self) -> None:
-        self.observations = None
-        self.agents = None 
+from hanabi_learning_environment import rl_env
 
+class TestHTGSAgent(unittest.TestCase):        
+
+    def setUp(self) -> None:
+        self.observation = self.set_observations()
+        self.agent = self.set_agent() 
 
     def set_observations(self):
-           
-        environment = rl_env.make('Hanabi-Full', num_players=5)
-        observations = environment.reset()
-         
+        
+        observed_hand_0 = [{'color': None, 'rank': -1}, 
+                           {'color': None, 'rank': -1}, 
+                           {'color': None, 'rank': -1}, 
+                           {'color': None, 'rank': -1}]
 
-        self.observations = observations
+        observed_hand_1 = [{'color': 'R', 'rank': 1}, 
+                           {'color': 'G', 'rank': 1}, 
+                           {'color': 'G', 'rank': 1}, 
+                           {'color': 'Y', 'rank': 2}]
+
+        observed_hand_2 = [{'color': 'G', 'rank': 2}, 
+                           {'color': 'W', 'rank': 3}, 
+                           {'color': 'Y', 'rank': 0}, 
+                           {'color': 'B', 'rank': 2}]
+
+        observed_hand_3 = [{'color': 'R', 'rank': 3},
+                           {'color': 'Y', 'rank': 1},
+                           {'color': 'G', 'rank': 0},
+                           {'color': 'B', 'rank': 3}]
+
+        observed_hand_4 = [{'color': 'Y', 'rank': 1},
+                           {'color': 'B', 'rank': 0},
+                           {'color': 'B', 'rank': 0},
+                           {'color': 'R', 'rank': 4}] 
+        
+        card_knowledge_hand = [{'color': None, 'rank': None}, 
+                               {'color': None, 'rank': None}, 
+                               {'color': None, 'rank': None}, 
+                               {'color': None, 'rank': None}]
+
+        
+        observations = {
+                        'current_player': 0, 
+                        'current_player_offset': 0, 
+                        'life_tokens': 3, 
+                        'information_tokens': 8, 
+                        'num_players': 5, 
+                        'deck_size': 30, 
+                        'fireworks': {'R': 0, 'Y': 0, 'G': 0, 'W': 0, 'B': 0}, 
+                        'legal_moves': [], 
+                        'legal_moves_as_int': [], 
+                        'observed_hands': [observed_hand_0, 
+                                           observed_hand_1, 
+                                           observed_hand_2, 
+                                           observed_hand_3, 
+                                           observed_hand_4], 
+                        'discard_pile': [], 
+                        'card_knowledge': [card_knowledge_hand.copy(), 
+                                           card_knowledge_hand.copy(), 
+                                           card_knowledge_hand.copy(), 
+                                           card_knowledge_hand.copy(), 
+                                           card_knowledge_hand.copy()], 
+                        }
+
+        return observations
     
-    def set_agents(self):
-        self.agents = [HTGSAgent({'players': 5}) \
-                                 for _ in range (5)] 
+    def set_agent(self):
+        agent = HTGSAgent({'players': 5})
                                  
-        for agent_idx, agent in enumerate (self.agents):
-            agent.init_table(self.observations['player_observations'][agent_idx])
-            agent.update_observation(self.observations['player_observations'][agent_idx])
-
-    def set_table(self):
-        self.table = Table(self.observation)    
-
+        
+        agent.init_table(self.observation)
+        agent.update_observation(self.observation)
+        
+        return agent
+    
     def test_duplicate_card_in_hand(self):
         """1. Testcase: card_idx = 3 duplicate"""
         # Init 
-        self.set_observations()
-        self.set_agents()
-        agent = self.agents[0]
-        
-    
 
+        
+        
         # Von der eigenen Hand ist die vierte Karte bekannt
         # Es ist eine Gelbe mit Rank 2
-        agent.table[0][3] = {'B': [0, 0, 0, 0, 0], 
-                             'G': [0, 0, 0, 0, 0], 
-                             'R': [0, 0, 0, 0, 0], 
-                             'W': [0, 0, 0, 0, 0], 
-                             'Y': [0, 0, 1, 0, 0]}
+        self.agent.table[0][3] = {'B': [0, 0, 0, 0, 0], 
+                            'G': [0, 0, 0, 0, 0], 
+                            'R': [0, 0, 0, 0, 0], 
+                            'W': [0, 0, 0, 0, 0], 
+                            'Y': [0, 0, 1, 0, 0]}
 
-        agent.observation['observed_hands'][1][0] = {'color': 'Y', 'rank': 2} 
-        poss_hand_table = agent.table[0]
+        self.agent.observation['observed_hands'][1][0] = {'color': 'Y', 'rank': 2} 
+        poss_hand_table = self.agent.table[0]
 
-        self.assertTrue(agent.duplicate_card_in_hand(poss_hand_table)
+        self.assertTrue(self.agent.duplicate_card_in_hand(poss_hand_table)
                         == {'color': 'Y', 'rank': 2})
 
-
-if __name__ == "__main__":
-    testClass = TestHTGSAgent()
-    testClass.test_duplicate_card_in_hand()
-
-                
+        
+    
