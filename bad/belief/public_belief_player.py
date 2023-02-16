13fee71f7e5d1abde33cb547320243b46871c068
Sascha Steger <s-steger@live.de>
2023-02-16 21:03:36 +0100

    Fixed Buges in Likelihood_hand_card Chance name in pub_belief

--- bad/belief/public_belief_player.py
+++ bad/belief/public_belief_player.py
@@ -15,9 +15,9 @@ class PublicBeliefPlayer(list):
              hint_matrix_ply: HintMatrixPlayer, 
              likelihood_ply: LikelihoodPlayer) -> list:
 
-        public_belief_player = [PublicBelfHandCard(constants, self.idx_ply, idx_card,
+        public_belief_hand_cards = [PublicBelfHandCard(constants, self.idx_ply, idx_card,
                                 rem_cards, hint_matrix_ply[idx_card], 
                                 likelihood_ply[idx_card]) 
                                 for idx_card in range(constants.num_ply)]
-        return public_belief_player
+        return public_belief_hand_cards
         
